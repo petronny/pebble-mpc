149a3db747a6b7fb2d744c78148e36c99bc38bf1
rjjacky <rjjacky@gmail.com>
2015-10-07 20:17:17 +0800

    new js app;

--- src/js/pebble-mpc.js
+++ src/js/pebble-mpc.js
@@ -1,13 +1,82 @@
-// Listen for when the watchface is opened
-Pebble.addEventListener('ready', 
+var W3CWebSocket = require('websocket').w3cwebsocket;
+ 
+//host = "192.168.1.173"
+host = "127.0.0.1"
+port = "9001"
+wsUrl = 'ws://'+host+':'+port+'/';
+var client = new W3CWebSocket(wsUrl);
+ 
+client.onerror = function() {
+    console.log('Connection Error');
+};
+ 
+client.onopen = function() {
+    console.log('WebSocket Client Connected');
+};
+ 
+client.onclose = function() {
+    console.log('echo-protocol Client Closed');
+};
+ 
+client.onmessage = function(e) {
+    if (typeof e.data === 'string') {
+        console.log("Received: '" + e.data + "'");
+    }
+};
+
+client.sendPebbleMessage = function (msg) {
+  var dictionary = {
+    "KEY_STATUS": 0,
+    "KEY_TITLE"::  "title",
+    "KEY_AUTHOR":  "author",
+    "KEY_CURRENT": 0,
+    "KEY_DURATION": 0,
+    "KEY_VOLUME": 0,
+  };
+
+  Pebble.sendAppMessage(dictionary,
+    fucntion(e) {
+      console.log(Status send successfully");
+    },
+    function(e) {
+      console.log(Error in sending");
+    }
+  );
+}
+
+var getStatus = function () {
+  client.send("status");
+}
+
+Pebble.addEventListener('ready',
   function(e) {
-    console.log('PebbleKit JS ready!');
+    console.log("Pebble redady");
+    getStatus();
   }
 );
 
-// Listen for when an AppMessage is received
+var sendQuery = function(client,payload) {
+     switch (payload["0"]) {
+      case 0:
+        client.send("0");
+        break;
+      case 1:
+        client.send("1");
+        break;
+      case 2:
+        client.send("2");
+        break;
+      case 3:
+        client.send("3");
+        break;
+      case 4:
+        client.send("4");
+        break;
+   }
+}
 Pebble.addEventListener('appmessage',
   function(e) {
-    console.log('AppMessage received!');
-  }                     
+    console.log("App Message received!" + JSON.stringify(e.payload) ); 
+    sendQuery(client,e.payload);
+  }
 );
